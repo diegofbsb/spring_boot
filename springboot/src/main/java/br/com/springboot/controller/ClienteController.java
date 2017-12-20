aa780255e6d6dc333253e15a0be1092f5d20fd53
admin <admin@DESKTOP-585OIQ2>
2017-12-19 22:47:25 -0200

    ccc

--- springboot/src/main/java/br/com/springboot/controller/ClienteController.java
+++ springboot/src/main/java/br/com/springboot/controller/ClienteController.java
@@ -1,11 +1,13 @@
 package br.com.springboot.controller;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +18,7 @@ import br.com.springboot.model.Cliente;
 @RestController
 public class ClienteController {
 	
-	Map<Integer, Cliente> clientes;
+	Map<Integer, Cliente> clientes = clientes = new HashMap<>();;
 	Integer proximoId = 0;
 	
 	//Negocio
@@ -31,11 +33,57 @@ public class ClienteController {
 		return cliente;
 	}
 	
+	private Collection<Cliente> buscarTodos(){
+		return clientes.values();
+		
+	}
 	
-	@RequestMapping(method=RequestMethod.POST, value="/clientes", consumes=MediaType.APPLICATION_JSON_VALUE)
+	public void excluir(Cliente cliente) {
+		clientes.remove(cliente.getId());
+		
+	}
+	
+	private Cliente buscaPorId(Integer id) {
+		return clientes.get(id);
+	}
+	
+	private Cliente alterar(Cliente cliente) {
+		clientes.put(cliente.getId(), cliente);
+		return cliente;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/clientes", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
 		Cliente clienteCadastrado = cadastrar(cliente);
 		System.out.println(cliente.getNome());
 		return new ResponseEntity<Cliente>(clienteCadastrado, HttpStatus.CREATED);
 	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/clientes", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<Cliente>> buscarTodosClientes() {
+		Collection<Cliente> clientesBuscados = buscarTodos();
+		return new ResponseEntity<>(clientesBuscados, HttpStatus.OK);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/clientes/{id}")
+	public ResponseEntity<Cliente> ecluirCliente(@PathVariable Integer id) {
+		
+		Cliente clienteEncontrado = buscaPorId(id);
+		
+		if(clienteEncontrado == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}else {
+			excluir(clienteEncontrado);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/clientes", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Cliente> alterarCliente(@RequestBody Cliente cliente) {
+		Cliente clienteAlterado = alterar(cliente);
+		return new ResponseEntity<>(clienteAlterado, HttpStatus.OK);
+	}
+	
+	
 }
